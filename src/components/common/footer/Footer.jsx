b870c3d0b9b3813fd0db1ff317c4c4a4d927d213
Yazuline <ingeniera-yazuline@hotmail.com>
2023-04-07 18:49:05 -0500

    adiciono formulario con validaciones, mejor estilo css

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: 'lightgray', padding: '20px', marginTop: '50px' , }}>
+    <footer style={{ backgroundColor: '#b0c4de', padding: '20px', marginTop: '50px' , }}>
     <div style={{ textAlign: 'center' }}>
       <p> Online</p>
       <p> &copy; 2023</p>
--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { Button, TextField } from '@mui/material';
+import styles from "./contac.module.css"
+
+
+const validationSchema = yup.object({
+  email: yup
+    .string('correo')
+    .email('Debe escribir un email')
+    .required('Email requerido'),
+  name: yup
+    .string('Escriba nombre completo')
+    .min(6, 'Nombre debe tener al menos 6 caracteres')
+    .required('Nombre requerdio'),
+});
 
 const Contact = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+     
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <div>Contact</div>
-  )
-}
+    <div className={styles.containerCards}>
+
+      <form onSubmit={formik.handleSubmit}>
+        
+      <TextField
+          fullWidth
+          id="name"
+          name="name"
+          type="text"
+          label="Nombre"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          error={formik.touched.name && Boolean(formik.errors.name)}
+          helperText={formik.touched.name && formik.errors.name}
+        />
+         <hr></hr>
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+         <hr></hr>
+        <Button color="primary" variant="contained" fullWidth type="submit">
+         ENVIAR
+        </Button>
+      </form>
+    </div>
+  );
+};
+
 
 export default Contact
